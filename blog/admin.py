42a22f0065615907d0565e634c52073c88cce850
GeSamaras <georgiosamaras@hotmail.com>
2025-02-02 03:36:51 +0100

    orm classes with examples, migration done, createsuperuser

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+# import all models to be registered
+from blog.models import Category, Comment, Post
 
 # Register your models here.
+
+# https://realpython.com/django-user-management/
+# createsuperuser
+# the way to give devs admin access to the project
+# creating, updating and deleting model classes from
+# a web gui
+# http://localhost:8000/admin
+
+# models from models.py also need to be registered in
+# admin.py, each model points to an admin class. 
+
+
+# defaults for now, but these classes can be coded
+# so they change in the admin page for visualization
+# and so on
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,14 @@ from django.db import models
 # this file works as an ORM, where classes become
 # database tables. classes are the columns, and 
 # instances are the rows for that data table. 
+# using SQLite by default, however firebase will be used. 
 
+# https://realpython.com/django-migrations-a-primer/
+# in order to update the ORM with new tables, run:
+# python manage.py makemigrations, while inside the project with (venv)
+
+# after makemigrations, to save them we need to run:
+# python manage.py migrate
 
 
 # these models all have their basic fields to be
@@ -17,20 +24,48 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+
 class Category(models.Model):
     name = models.CharField(max_length=30)
+
+    # this will override the model name in admin's dashboard
+    # for better readability
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField() # TextField() makes so body holds a dynamic string length 
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    # https://docs.djangoproject.com/en/5.1/ref/models/fields/#django.db.models.ManyToManyField
     categories = models.ManyToManyField("Category", related_name="posts")
+
+    def __str__(self):
+        return self.title
+
+
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    # ForeignKey() is also similar to ManyToManyField
+    # since many posts can be made by the same one user
+    # this method defines a Many-to-one relationship where
+    # the user is the owner of many post models. 
+    # two parameters, the relationship, and what django does
+    # once the post is deleted. It will remove all child posts
+    # to that specific post. 
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
 
-# Create your models here.
+    # these dunder str functions serve to better represent
+    # the models, by return a formatted version of their
+    # fields, names, contents and such
+    def __str__(self):
+        return f"{self.author} on '{self.post}'"
